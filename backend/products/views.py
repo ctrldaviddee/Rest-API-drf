486ae77e3476b00698af890fabe119d8424d71fa
ctrldaviddee <princedavealex.20@gmail.com>
2025-11-03 19:25:20 +0100

    Custom Permissions

--- backend/products/views.py
+++ backend/products/views.py
@@ -11,7 +11,7 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.DjangoModelPermissions]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self, serializer):
         #serializer.save(user=self.request.user)
