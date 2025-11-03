9473399b3482c85437ecfc564541ac090540620a
ctrldaviddee <princedavealex.20@gmail.com>
2025-11-03 11:39:44 +0100

    DRF CreateApiView

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,6 +3,20 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        #serializer.save(user=self.request.user)
+        print('serializer validated data = ', serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+        # send a Django signal here
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
