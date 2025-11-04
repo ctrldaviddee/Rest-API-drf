fc98c2d1a18da50afe10c9b642670e526c47da85
ctrldaviddee <princedavealex.20@gmail.com>
2025-11-04 15:43:23 +0100

    URLs, Reverse, & Serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,15 +1,56 @@
 from rest_framework import serializers
-
+from rest_framework.reverse import reverse
 from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
     discount = serializers.SerializerMethodField(read_only=True)
-
+    url = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True) # works anywhere
+    an_url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk',
+    ) # The HyperLinkedIdentityField only works on a model Serializer
     class Meta:
         model = Product
-        fields = ['id', 'title', 'content', 'price', 'sale_price', 'discount',]
-        #fields = '__all__'
+        fields = [
+            'an_url',
+            'url',
+            'edit_url',
+            'id',
+            'title',
+            'content',
+            'price',
+            'sale_price',
+            'discount',
+        ]
+        # fields = '__all__'
+
+
+    def get_url(self, obj):
+        request = self.context.get('request')
+
+        if request is None:
+            return None
+
+        return reverse(viewname='product-detail', kwargs={'pk': obj.pk}, request=request)
+
+
+    def get_edit_url(self, obj):
+        request = self.context.get('request')
+
+        if request is None:
+            return None
+
+        return reverse(viewname='product-edit', kwargs={'pk': obj.pk}, request=request)
+
+    def get_an_url(self, obj):
+            request = self.context.get('request')
+
+            if request is None:
+                return None
+
+            return reverse(viewname='product-detail', kwargs={'pk': obj.pk}, request=request)
 
     def get_discount(self, obj):
         if not hasattr(obj, 'id'):
