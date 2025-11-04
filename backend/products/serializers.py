3fbd231c09a1f9f65217ca1468b60563b6e2a3d7
ctrldaviddee <princedavealex.20@gmail.com>
2025-11-04 22:46:28 +0100

    Related Fields & Foreign Keys

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,24 +1,38 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
+from api.serializers import UserPublicSerializer
+
 from .models import Product
 from .validators import validate_title_no_hello, unique_product_title
 
-class ProductSerializer(serializers.ModelSerializer):
-    discount = serializers.SerializerMethodField(read_only=True)
-    url = serializers.SerializerMethodField(read_only=True)
-    edit_url = serializers.SerializerMethodField(read_only=True) # works anywhere
+
+class ProductInlineSerializer(serializers.Serializer):
     an_url = serializers.HyperlinkedIdentityField(
         view_name='product-detail',
         lookup_field='pk',
-    ) # The HyperLinkedIdentityField only works on a model Serializer
+        read_only=True
+    )
+    title = serializers.CharField(read_only=True)
+
+class ProductSerializer(serializers.ModelSerializer):
+    owner = UserPublicSerializer(source='user', read_only=True)
+    related_products = ProductInlineSerializer(source='user.product_set.all', read_only=True, many=True)
+    discount = serializers.SerializerMethodField(read_only=True)
+    url = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)  # works anywhere
+    an_url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk',
+    )  # The HyperLinkedIdentityField only works on a model Serializer
 
     # email = serializers.EmailField(write_only=True)
 
     title = serializers.CharField(validators=[validate_title_no_hello, unique_product_title])
+
     class Meta:
         model = Product
         fields = [
-            # 'user',
+            'owner',
             'an_url',
             'url',
             'edit_url',
@@ -29,27 +43,28 @@ class ProductSerializer(serializers.ModelSerializer):
             'price',
             'sale_price',
             'discount',
+           'related_products',
         ]
         # fields = '__all__'
 
-#    def validate_title(self, value):
-#        qs = Product.objects.filter(title__iexact=value)
-#        if qs.exists():
-#            raise serializers.ValidationError('This title has already been used.')
-#        return value
+    #    def validate_title(self, value):
+    #        qs = Product.objects.filter(title__iexact=value)
+    #        if qs.exists():
+    #            raise serializers.ValidationError('This title has already been used.')
+    #        return value
 
     # not really practical
-    #def create(self, validated_data):
-        # return Product.objects.create(**validated_data)
-        # email = validated_data.pop('email')
+    # def create(self, validated_data):
+    # return Product.objects.create(**validated_data)
+    # email = validated_data.pop('email')
     #    obj = super().create(validated_data)
-        # print(email, obj)
+    # print(email, obj)
     #    return obj
 
-    #def update(self, instance, validated_data):
+    # def update(self, instance, validated_data):
     #    email = validated_data.pop('email')
-        # instance.title = validated_data.get('title')
-        # return instance
+    # instance.title = validated_data.get('title')
+    # return instance
     #    return super().update(instance, validated_data)
 
     def get_url(self, obj):
@@ -60,7 +75,6 @@ class ProductSerializer(serializers.ModelSerializer):
 
         return reverse(viewname='product-detail', kwargs={'pk': obj.pk}, request=request)
 
-
     def get_edit_url(self, obj):
         request = self.context.get('request')
 
@@ -70,12 +84,12 @@ class ProductSerializer(serializers.ModelSerializer):
         return reverse(viewname='product-edit', kwargs={'pk': obj.pk}, request=request)
 
     def get_an_url(self, obj):
-            request = self.context.get('request')
+        request = self.context.get('request')
 
-            if request is None:
-                return None
+        if request is None:
+            return None
 
-            return reverse(viewname='product-detail', kwargs={'pk': obj.pk}, request=request)
+        return reverse(viewname='product-detail', kwargs={'pk': obj.pk}, request=request)
 
     def get_discount(self, obj):
         if not hasattr(obj, 'id'):
